c3f3285e5b75de10ea6ccf663afdd1445bd89515
Vlad Vartolomei <vlad.vartolomei@gmail.com>
2019-05-24 23:28:34 +0300

    caesar cypher - encryption & decryption - green

--- src/main/scala/com/vladv/crypto/CaesarCypher.scala
+++ src/main/scala/com/vladv/crypto/CaesarCypher.scala
@@ -3,11 +3,18 @@ package com.vladv.crypto
 object CaesarCypher {
 
   def encrypt(plain :String, key : Int) :String = {
-    ""
+    plain
+      .map(_.toUpper.toInt)
+      .map( asciiCode => ((asciiCode - 'A'.toInt) + key) % 26 + 'A'.toInt)
+      .foldLeft("")( _ :+ _.toChar)
   }
 
   def decrypt(encrypted :String, key :Int) :String = {
-    ""
+    encrypted
+      .map(_.toUpper.toInt)
+      .map( asciiCode =>((((asciiCode - 'A'.toInt) - key) % 26) + 26) % 26)
+      .map( asciiInt => asciiInt + 'A'.toInt)
+      .foldLeft("")( _ :+ _.toChar)
   }
 
 }
--- src/test/scala/com/vladv/crypto/CaesarTest.scala
+++ src/test/scala/com/vladv/crypto/CaesarTest.scala
@@ -6,7 +6,20 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class CaesarTest extends FunSuite {
-  test("Caesar cypher for ASDASDASDAS is QWEQWEQWEQW with key = 5") {
-    assert("QWEQWEQWEQW" == CaesarCypher.encrypt("ASDASDASDAS", 5))
+  test("Caesar cypher for ASDASDASDAS is FXIFXIFXIFX with key = 5") {
+    assert("FXIFXIFXIFX" == CaesarCypher.encrypt("ASDASDASDAS", 5))
   }
+
+  test("Caesar cypher for CRIPTOGRAFIE is QWEQWEQWEQW with key = 7") {
+    assert("JYPWAVNYHMPL" == CaesarCypher.encrypt("CRIPTOGRAFIE", 7))
+  }
+
+  test("Caesar decypher for FXIFXIFXIFX is ASDASDASDAS with key = 5") {
+    assert("ASDASDASDAS" == CaesarCypher.decrypt("FXIFXIFXIFX", 5))
+  }
+
+  test("Caesar decypher for JYPWAVNYHMPL is CRIPTOGRAFIE with key = 7") {
+    assert("CRIPTOGRAFIE" == CaesarCypher.decrypt("JYPWAVNYHMPL", 7))
+  }
+
 }
