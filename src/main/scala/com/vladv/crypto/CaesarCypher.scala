179db819929544e0d7a25223f7a7fd2d7d547bfc
Vlad Vartolomei <vlad.vartolomei@gmail.com>
2019-05-25 00:38:16 +0300

    generic decrypt, clean-up

--- src/main/scala/com/vladv/crypto/CaesarCypher.scala
+++ src/main/scala/com/vladv/crypto/CaesarCypher.scala
@@ -2,19 +2,27 @@ package com.vladv.crypto
 
 object CaesarCypher {
 
+  def maxAlphabetLength :Int = 26
+
+  private def positiveModulo(x :Int, n :Int) : Int = (x % n + n) % n
+
   def encrypt(plain :String, key : Int) :String = {
     plain
       .map(_.toUpper.toInt)
-      .map( asciiCode => ((asciiCode - 'A'.toInt) + key) % 26 + 'A'.toInt)
-      .foldLeft("")( _ :+ _.toChar)
+      .map( asciiCode => positiveModulo(asciiCode - 'A'.toInt + key, maxAlphabetLength) + 'A'.toInt toChar)
+      .foldLeft("")( _ + _ )
   }
 
   def decrypt(encrypted :String, key :Int) :String = {
     encrypted
       .map(_.toUpper.toInt)
-      .map( asciiCode =>((((asciiCode - 'A'.toInt) - key) % 26) + 26) % 26)
-      .map( asciiInt => asciiInt + 'A'.toInt)
-      .foldLeft("")( _ :+ _.toChar)
+      .map( asciiCode => positiveModulo(asciiCode - 'A'.toInt - key, maxAlphabetLength) + 'A'.toInt toChar)
+      .foldLeft("")( _ + _ )
+  }
+
+  def decrypt(encrypted :String) :String = {
+    ( 0 until maxAlphabetLength).map(decrypt(encrypted, _))
+    .foldLeft("")( _ + "\n" +_ )
   }
 
 }
